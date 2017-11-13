d23ca7b774191606eec5f5026a7504821d49392c
Anda <ax.fiolle@gmail.com>
2017-11-13 14:11:50 +0100

    Auto stash before merge of "master" and "origin/master"

--- Controller/chatController.php
+++ Controller/chatController.php
@@ -1,6 +1,21 @@
 <?php
   class chatController {
-  public function __construct(){
+    public function __construct(){
+    /*private function getUserId($username) {
+      $do = "SELECT ID FROM my_users WHERE username = '".$username."'";
+      try {
+        $try = $db->query($do); $try = $try->fetch(); $try = $try[0];
+        return $try;
+      } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();}
+    
+    private function getTimePost($user_id) {
+      $do = "SELECT datepost FROM shoutbox WHERE user_id = '".$user_id."' ORDER BY id DESC";
+      try {
+        $try = $db->query($do); $try = $try->fetch(); $try = $try[0];
+        return $try;
+      } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();}
+    }*/
+}
   }
   public function run(){
     include View . 'chat.php';
--- Controller/registerController.php
+++ Controller/registerController.php
@@ -1,6 +1,12 @@
 <?php
   class loginController {
   public function __construct(){
+    /*private function addUser($username,$mail,$pass) {
+    global $db;
+    try {
+      $db->exec("INSERT INTO my_users (username,mail,pass) VALUES ('".$username."','".$mail."','".$pass."')");
+    } catch(Exception $e) {echo("Error addUser : ".$e->getMessage());die();}*/
+}
   }
   public function run(){
     include View . 'register.php';
