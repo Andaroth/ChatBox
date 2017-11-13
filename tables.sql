b78e83750cb244a1b37879893cf8b8bc46c987ca
Anda <ax.fiolle@gmail.com>
2017-11-13 14:01:45 +0100

    +tables.sql

---
+++ tables.sql
@@ -0,0 +1,30 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `my_users` (
+  `ID` int(11) NOT NULL,
+  `username` text COLLATE utf8_bin NOT NULL,
+  `mail` text COLLATE utf8_bin NOT NULL,
+  `pass` text COLLATE utf8_bin NOT NULL,
+  `auth` int(11) NOT NULL DEFAULT '0',
+  `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+CREATE TABLE `shoutbox` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` text COLLATE utf8_bin NOT NULL,
+  `datepost` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+ALTER TABLE `my_users`
+  ADD PRIMARY KEY (`ID`);
+
+ALTER TABLE `shoutbox`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `my_users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+ALTER TABLE `shoutbox`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
