41a32f9bbd7f0f6b651814d0d495b7af2ec9b28f
Anda <ax.fiolle@gmail.com>
2017-11-13 14:18:17 +0100

    controllers update

--- Controller/chatController.php
+++ Controller/chatController.php
@@ -1,12 +1,22 @@
 <?php
   class chatController {
   public function __construct(){
-      /*private function getUserId($username) { 
+    /*
+    private function getUserId($username) { 
       $do = "SELECT ID FROM my_users WHERE username = '".$username."'"; 
       try { 
         $try = $db->query($do); $try = $try->fetch(); $try = $try[0]; 
         return $try; 
       } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();} 
+      
+    private function getUserById($thisID) {
+      global $db;
+      $do = "SELECT username FROM my_users WHERE ID = '".$thisID."'";
+      try {
+        $try = $db->query($do); $try = $try->fetch(); $try = $try[0];
+        return $try;
+      } catch(Exception $e) {echo("Error getUserById : ".$e->getMessage());die();}
+    }
      
     private function getTimePost($user_id) { 
       $do = "SELECT datepost FROM shoutbox WHERE user_id = '".$user_id."' ORDER BY id DESC"; 
@@ -14,7 +24,15 @@
         $try = $db->query($do); $try = $try->fetch(); $try = $try[0]; 
         return $try; 
       } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();} 
-    }*/ 
+    }
+    
+    private function addChat($user_id,$message) {
+      global $db;
+      try {
+        $db->exec("INSERT INTO shoutbox (user_id,message) VALUES ('".$user_id."','".$message."')");
+      } catch(Exception $e) {echo("Error addChat : ".$e->getMessage());die();}
+    }
+    */ 
   } 
   }
   public function run(){
--- Model/global.php
+++ Model/global.php
@@ -1,8 +1,8 @@
 <?php
-/* DataBase */
+/*DataBase*/
 $dbhost = "localhost";
 $dbname = "ChatBox";
 $dbuser = "root";
 $dbpass = "user";
-/* SQL Queries */
+/* SQL Queries */ 
 $getChat = $db->query("SELECT * FROM shoutbox ORDER BY datepost DESC LIMIT 10");
