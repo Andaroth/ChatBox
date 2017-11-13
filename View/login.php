1108f5f77ca8ec0e05251cfeb8bb1b436ccba5c3
Mutwakil Mo <mutwakil.m.osman@gmail.com>
2017-11-13 12:36:05 +0100

    login and register

--- View/login.php
+++ View/login.php
@@ -1,1 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="view/css/style.css">
+  <title>Chatbox</title>
+</head>
+<body>
+  <?php
+session_start();
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$con = mysql_connect('localhost', 'root', '');
+mysql_select_db('chatbox', $con);
+
+$result = mysql_query("SELECT * FROM users WHERE username='$username' AND password='$password'");
+
+if(mysql_num_rows($result)){
+$res = mysql_fetch_array($result);
+
+$_SESSION['username'] = $res['username'];
+echo "You are now Logged in. Click <a href='index.php'>here</a> to go back to main chat window.";
+}
+
+else{
+
+echo "No user found. Please go <a href='index.php'>back</a> and enter the correct login.<br>";
+echo "You may register a new account by clicking <a href='register.php'>here</a>.";
+
+}
+
+?>
+</body>
+</html>
+
+
+
+
 <?php echo "login" ?>
