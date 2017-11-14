b52f51f23f5bf8dddd5a289ca953ce91a4983df3
Anda <ax.fiolle@gmail.com>
2017-11-14 15:20:05 +0100

    fix

--- View/login.php
+++ View/login.php
@@ -4,11 +4,9 @@
       <h2>Se connecter</h2>
       <p><?php echo $alertMsg; ?></p>
       <form action="./?page=login" method="post">
-        <div class="twocol">
-          <div>
-            <input class="clearform first inputupper" type="text" name="username" id="login_username" placeholder="username">
-            <input class="clearform" type="password" name="pass" id="login_pass" placeholder="password">
-          </div>
+        <div>
+          <input class="clearform first inputupper" type="text" name="username" id="login_username" placeholder="username">
+          <input class="clearform" type="password" name="pass" id="login_pass" placeholder="password">
         </div>
         <input type="submit" value="Connexion">
       </form>
