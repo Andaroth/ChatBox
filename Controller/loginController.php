f53e693127e1e8e7ac8fc825f81fa97c513874ef
Anda <ax.fiolle@gmail.com>
2017-11-14 10:22:13 +0100

    verbose

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -8,6 +8,7 @@
       $select = $DB->query("SELECT COUNT(*) AS count FROM my_users WHERE username = '".$username."' AND pass = '".$pass."'");
       $row = $select->fetch();
       $count = $row['count'];
+        echo "aaaaaaaaaaah !! form ok";
     }
     else {
       $username = "Guest";
@@ -15,6 +16,7 @@
       $count = -1;
         
       $alertMsg = "Veuillez vous connecter avec vos identifiants.";
+        echo "aaaaaaaaaaah !! rien";
     }
                    
     if($count==1) {
@@ -23,12 +25,14 @@
       $_SESSION["user_id"] = intval(getUserId($username));
         
       $alertMsg = "Tu t'es connecté comme il faut !";
+        echo "aaaaaaaaaaah !! oké tu pe rentray";
         
       // Il faut soit rediriger le user, soit faire toute la page en controller. 
       // header("Refresh: 3; http://".$_SERVER['HTTP_HOST']."/");
     }
     else {
       $alertMsg = "Mot de passe ou nom d'utilisateur incorrect !";
+        echo "aaaaaaaaaaah !! oké tu pe PAS rentray";
     }
   }
   public function run(){
