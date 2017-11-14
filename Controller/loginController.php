4c5a6bb58a9c450f2932e05b0980bf1243ed8188
Marvin-LOUIS <marvinlouis@idea-box.tech>
2017-11-14 10:28:36 +0100

    It's alive

--- Controller/loginController.php
+++ Controller/loginController.php
@@ -1,14 +1,14 @@
 <?php
   class loginController {
   public function __construct(){
-    if ( (isset($_POST['username'])) && (isset($_POST['pass'])) && (isset($_POST['submit'])) )
+    global $DB, $alertMsg;
+    if ( (isset($_POST['username'])) && (isset($_POST['pass'])))
     {
       $username = $_POST['username'];
       $pass = hash("sha256", htmlspecialchars($_POST['pass']));
       $select = $DB->query("SELECT COUNT(*) AS count FROM my_users WHERE username = '".$username."' AND pass = '".$pass."'");
       $row = $select->fetch();
       $count = $row['count'];
-        echo "aaaaaaaaaaah !! form ok";
     }
     else {
       $username = "Guest";
@@ -16,7 +16,6 @@
       $count = -1;
         
       $alertMsg = "Veuillez vous connecter avec vos identifiants.";
-        echo "aaaaaaaaaaah !! rien";
     }
                    
     if($count==1) {
@@ -25,14 +24,12 @@
       $_SESSION["user_id"] = intval(getUserId($username));
         
       $alertMsg = "Tu t'es connecté comme il faut !";
-        echo "aaaaaaaaaaah !! oké tu pe rentray";
         
       // Il faut soit rediriger le user, soit faire toute la page en controller. 
-      // header("Refresh: 3; http://".$_SERVER['HTTP_HOST']."/");
+      header("Refresh: 3; http://".$_SERVER['HTTP_HOST']."/?page=chat");
     }
     else {
       $alertMsg = "Mot de passe ou nom d'utilisateur incorrect !";
-        echo "aaaaaaaaaaah !! oké tu pe PAS rentray";
     }
   }
   public function run(){
