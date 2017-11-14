7a539e5574e9ce457b28df6399f121b2b35f01de
Anda <ax.fiolle@gmail.com>
2017-11-14 10:15:52 +0100

    added functions to global

--- Controller/chatController.php
+++ Controller/chatController.php
@@ -3,13 +3,6 @@
   public function __construct(){
     
   }
-  private function getUserId($username) { 
-    $do = "SELECT ID FROM my_users WHERE username = '".$username."'"; 
-    try { 
-      $try = $DB->query($do); $try = $try->fetch(); $try = $try[0]; 
-      return $try; 
-    } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();} 
-  }
 
   private function getTimePost($user_id) { 
     $do = "SELECT datepost FROM shoutbox WHERE user_id = '".$user_id."' ORDER BY id DESC"; 
--- Model/functions.php
+++ Model/functions.php
@@ -1,4 +1,12 @@
 <?php
+  function getUserId($username) { 
+    $do = "SELECT ID FROM my_users WHERE username = '".$username."'"; 
+    try { 
+      $try = $DB->query($do); $try = $try->fetch(); $try = $try[0]; 
+      return $try; 
+    } catch(Exception $e) {echo("Error getUserId : ".$e->getMessage());die();} 
+  }
+
   function getUserById($thisID) {
     global $DB;
     $do = "SELECT username FROM my_users WHERE ID = '".$thisID."'";
